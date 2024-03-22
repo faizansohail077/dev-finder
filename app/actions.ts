972dd99eced7658d512a0e9f6b4d1b72c9a37030
Faizan <faizansohail077@gmail.com>
2024-03-23 01:27:00 +0500

    removed use server

--- app/actions.ts
+++ app/actions.ts
@@ -4,7 +4,6 @@ import { getsessions } from "@/lib/auth"
 import deleteUser from "./data-access/users"
 
 export const deleteAccount = async () => {
-    "use server"
     const session = await getsessions()
     if (!session) {
         return alert("UnAuthorized")
