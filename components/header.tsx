6850b128c28500c3e92f5b070774f1b60afec514
Faizan <faizansohail077@gmail.com>
2024-03-28 22:06:00 +0500

    cleaned code

--- components/header.tsx
+++ components/header.tsx
@@ -21,7 +21,6 @@ import {
     AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle,
-    AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { useState } from "react";
 import { deleteAccount } from "@/app/actions";
